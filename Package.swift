54e1d99bd1dd162319055758df1a97bef0d2f29e
Quico Moya <me@qmoya.com>
2023-03-10 23:51:30 +0100

    Relax the package’s macOS version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
 	name: "Snippetize",
-	platforms: [.macOS(.v13)],
+	platforms: [.macOS(.v12)],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
 	],
